05ba4a2d14d353ea439ae2374928eba45ae7bcc5
Stephan Rave <stephanrave@uni-muenster.de>
2016-01-06 10:26:07 +0100

    [operators.nedelec] add L2ProductFunctional, InterpolationOperator, CenterEvaluation

--- src/pymor/operators/nedelec.py
+++ src/pymor/operators/nedelec.py
@@ -13,6 +13,7 @@ import numpy as np
 from scipy.sparse import coo_matrix, csc_matrix
 
 from pymor.functions.interfaces import FunctionInterface
+from pymor.grids.boundaryinfos import EmptyBoundaryInfo
 from pymor.grids.referenceelements import triangle
 from pymor.operators.numpy import NumpyMatrixBasedOperator
 from pymor.parameters.base import Parametric
@@ -210,6 +211,137 @@ class L2ProductOperator(NumpyMatrixBasedOperator):
 
         self.logger.info('Assemble system matrix ...')
         A = coo_matrix((M, (SF_I0, SF_I1)), shape=(g.size(1), g.size(1)))
+        del M, SF_I0, SF_I1
+        A = csc_matrix(A).copy()
+
+        # The call to copy() is necessary to resize the data arrays of the sparse matrix:
+        # During the conversion to crs_matrix, entries corresponding with the same
+        # coordinates are summed up, resulting in shorter data arrays. The shortening
+        # is implemented by calling self.prune() which creates the view self.data[:self.nnz].
+        # Thus, the original data array is not deleted and all memory stays allocated.
+
+        return A
+
+
+class L2ProductFunctional(NumpyMatrixBasedOperator):
+
+    sparse = False
+    range = NumpyVectorSpace(1)
+
+    def __init__(self, grid, function, boundary_info=None, dirichlet_data=None, name=None):
+        assert grid.reference_element is triangle
+        assert function.dim_domain == 2 and function.shape_range == (2,)
+        assert dirichlet_data is None \
+            or (dirichlet_data.dim_domain == 2 and dirichlet_data.shape_range == (2,))
+        self.source = NumpyVectorSpace(grid.size(1))
+        self.grid = grid
+        self.boundary_info = boundary_info
+        self.function = function
+        self.dirichlet_data = dirichlet_data
+        self.name = name
+        self.build_parameter_type(inherits=(function, dirichlet_data))
+
+    def _assemble(self, mu=None):
+        g = self.grid
+        bi = self.boundary_info
+
+        I = InterpolationOperator(g, self.function).assemble(mu)._matrix.ravel()
+        M = L2ProductOperator(self.grid, EmptyBoundaryInfo(self.grid)).assemble()._matrix
+        I = M.dot(I)
+        del M
+
+        if bi is not None and bi.has_dirichlet:
+            DI = bi.dirichlet_boundaries(1)
+            if self.dirichlet_data is not None:
+                SE = g.subentities(1, 2)[DI]
+                CO = g.centers(2)[SE]
+                TANGENTS = (CO[:, 0] - CO[:, 1]) * np.sign(SE[:, 0] - SE[:, 1])[:, np.newaxis] / g.volumes(1)[DI, np.newaxis]
+                I[DI] = np.sum(self.dirichlet_data(g.centers(1)[DI], mu=mu) * TANGENTS, axis=-1)
+            else:
+                I[DI] = 0
+
+        return I.reshape((1, -1))
+
+
+class InterpolationOperator(NumpyMatrixBasedOperator):
+
+    source = NumpyVectorSpace(1)
+    linear = True
+
+    def __init__(self, grid, function):
+        assert grid.reference_element is triangle
+        assert function.dim_domain == 2
+        assert function.shape_range == (2,)
+        self.grid = grid
+        self.function = function
+        self.range = NumpyVectorSpace(grid.size(1))
+        self.build_parameter_type(inherits=(function,))
+
+    def _assemble(self, mu=None):
+        g = self.grid
+        SE = g.subentities(1, 2)
+        CO = g.centers(2)[SE]
+        TANGENTS = (CO[:, 0] - CO[:, 1]) * np.sign(SE[:, 0] - SE[:, 1])[:, np.newaxis] / g.volumes(1)[:, np.newaxis]
+        return np.sum(self.function(g.centers(1), mu=mu) * TANGENTS, axis=-1).reshape((-1, 1))
+
+
+class CenterEvaluation(NumpyMatrixBasedOperator):
+
+    linear = True
+
+    def __init__(self, grid):
+        assert grid.reference_element is triangle
+        self.grid = grid
+        self.source = NumpyVectorSpace(grid.size(1))
+        self.range = NumpyVectorSpace(grid.size(0) * 2)
+
+    def _assemble(self, mu=None):
+        g = self.grid
+        M = np.empty((g.size(0), 2, 3))
+        edge_lengths = g.volumes(1)[g.subentities(0, 1)]
+        CO = g.centers(2)[g.subentities(0, 2)]
+
+        M[:, 0, 2] = ((np.sqrt(np.power(CO[:, 0, 0] - CO[:, 1, 0],2) + np.power(CO[:, 0, 1] - CO[:, 1, 1],2))*(CO[:, 0, 1] + CO[:, 1, 1] - 2*CO[:, 2, 1]))/
+            (3.*(CO[:, 0, 1]*(CO[:, 1, 0] - CO[:, 2, 0]) + CO[:, 1, 1]*CO[:, 2, 0] - CO[:, 1, 0]*CO[:, 2, 1] + CO[:, 0,
+                0]*(-CO[:, 1, 1] + CO[:, 2, 1]))))
+
+        M[:, 1, 2] =  ( -(np.sqrt(np.power(CO[:, 0, 0] - CO[:, 1, 0],2) + np.power(CO[:, 0, 1] - CO[:, 1, 1],2))*(CO[:, 0, 0] + CO[:, 1, 0] - 2*CO[:, 2, 0]))/
+            (3.*(CO[:, 0, 1]*(CO[:, 1, 0] - CO[:, 2, 0]) + CO[:, 1, 1]*CO[:, 2, 0] - CO[:, 1, 0]*CO[:, 2, 1] + CO[:, 0,
+                0]*(-CO[:, 1, 1] + CO[:, 2, 1]))))
+
+
+        M[:, 0, 1] = - ((np.sqrt(np.power(CO[:, 0, 0] - CO[:, 2, 0],2) + np.power(CO[:, 0, 1] - CO[:, 2, 1],2))*(CO[:, 0, 1] - 2*CO[:, 1, 1] + CO[:, 2, 1]))/
+            (3.*(-(CO[:, 1, 1]*CO[:, 2, 0]) + CO[:, 0, 1]*(-CO[:, 1, 0] + CO[:, 2, 0]) + CO[:, 0, 0]*(CO[:, 1, 1] - CO[:, 2, 1])
+                + CO[:, 1, 0]*CO[:, 2, 1])))
+
+        M[:, 1, 1] = - (((CO[:, 0, 0] - 2*CO[:, 1, 0] + CO[:, 2, 0])*np.sqrt(np.power(CO[:, 0, 0] - CO[:, 2, 0],2) + np.power(CO[:, 0, 1] - CO[:, 2, 1],2)))/
+            (3.*(CO[:, 0, 1]*(CO[:, 1, 0] - CO[:, 2, 0]) + CO[:, 1, 1]*CO[:, 2, 0] - CO[:, 1, 0]*CO[:, 2, 1] + CO[:, 0,
+                0]*(-CO[:, 1, 1] + CO[:, 2, 1]))))
+
+
+        M[:, 0, 0] = ((np.sqrt(np.power(CO[:, 1, 0] - CO[:, 2, 0],2) + np.power(CO[:, 1, 1] - CO[:, 2, 1],2))*(-2*CO[:, 0, 1] + CO[:, 1, 1] + CO[:, 2, 1]))/
+            (3.*(CO[:, 0, 1]*(CO[:, 1, 0] - CO[:, 2, 0]) + CO[:, 1, 1]*CO[:, 2, 0] - CO[:, 1, 0]*CO[:, 2, 1] + CO[:, 0,
+                0]*(-CO[:, 1, 1] + CO[:, 2, 1]))))
+
+        M[:, 1, 0] = (((-2*CO[:, 0, 0] + CO[:, 1, 0] + CO[:, 2, 0])*np.sqrt(np.power(CO[:, 1, 0] - CO[:, 2, 0],2) + np.power(CO[:, 1, 1] - CO[:, 2, 1],2)))/
+        (3.*(-(CO[:, 1, 1]*CO[:, 2, 0]) + CO[:, 0, 1]*(-CO[:, 1, 0] + CO[:, 2, 0]) + CO[:, 0, 0]*(CO[:, 1, 1] - CO[:, 2, 1]) +
+            CO[:, 1, 0]*CO[:, 2, 1])))
+
+        SIGNS = np.empty((g.size(0), 3))
+        SE = g.subentities(0, 2)
+        SIGNS[:, 0] = np.sign(SE[:, 2] - SE[:, 1])
+        SIGNS[:, 1] = np.sign(SE[:, 0] - SE[:, 2])
+        SIGNS[:, 2] = np.sign(SE[:, 1] - SE[:, 0])
+
+        M *= SIGNS[:, np.newaxis, :]
+
+        self.logger.info('Determine global dofs ...')
+        M = M.ravel()
+        SF_I0 = np.repeat(np.arange(g.size(0) * 2), 3)
+        SF_I1 = np.tile(g.subentities(0, 1), [1, 2]).ravel()
+
+        self.logger.info('Assemble system matrix ...')
+        A = coo_matrix((M, (SF_I0, SF_I1)), shape=(g.size(0) * 2, g.size(1)))
         del M, SF_I0, SF_I1
         A = csc_matrix(A).copy()
 
